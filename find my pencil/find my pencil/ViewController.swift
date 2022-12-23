11c7230ef32da3cfd9a1c2c0c4666a1fcae424fe
SylvainJunca <sylvain.junca@gmail.com>
2022-12-22 22:59:29 -0500

    scans blutooth around

--- find my pencil/find my pencil/ViewController.swift
+++ find my pencil/find my pencil/ViewController.swift
@@ -9,20 +9,51 @@ import UIKit
 import CoreBluetooth
 
 class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var centralManager: CBCentralManager!
+    var myPeripheral: CBPeripheral!
+    struct Peripheral {
+        var name: String
+        var RSSI: NSNumber
+        var advertisementData: [String : Any]
+    }
+    var peripherals = [Peripheral]()
+    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         if central.state == CBManagerState.poweredOn {
-            print("BLE powered on")
-                // Turned on
+            central.scanForPeripherals(withServices: nil, options: nil)
             }
             else {
                 print("Something wrong with BLE")
                 // Not on, but can have different issues
             }
-        
+
     }
     
-    var centralManager: CBCentralManager!
-    var myPeripheral: CBPeripheral!
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+       
+        if let pname = peripheral.name {
+            print(pname, RSSI)
+            if let index = peripherals.firstIndex(where: { $0.name == pname
+            }) {
+                var periph = peripherals[index]
+                periph.RSSI = RSSI
+                peripherals[index] = periph
+            } else {
+                let periph = Peripheral(name: pname, RSSI: RSSI, advertisementData: advertisementData)
+                peripherals.append(periph)
+                
+            }
+            print(peripherals.count)
+            
+           
+        }
+        if let pdelegate = peripheral.services {
+            print( pdelegate)
+        }
+
+    }
+
     
 
     override func viewDidLoad() {
